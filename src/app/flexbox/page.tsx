23eb78a2008e7b4d340a6e8389cceb081a5d8c94
Demetrius Carregosa <demetriuscarregosa@gmail.com>
2025-02-18 22:05:39 -0300

    test coding

---
+++ src/app/flexbox/page.tsx
@@ -0,0 +1,18 @@
+export default function Page() {
+    return (
+        <div className="h-96 w-[900px]">
+            <h1>Flexbox</h1>
+            <div
+                className="
+                        bg-zinc-700
+                        h-96
+                        flex
+                        justify-around
+                        items-start
+                        gap-5
+                    "
+            >             
+            </div>
+        </div>
+    )
+}
