760d5c404ad82adc6b85681052e413719348cea2
Demetrius Carregosa <demetriuscarregosa@gmail.com>
2025-03-01 12:26:15 -0300

    add icons react

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 - Next
 - Taiwind
+- Icons React
--- src/components/template/MenuPrincipal.tsx
+++ src/components/template/MenuPrincipal.tsx
@@ -1,14 +1,24 @@
 
+import { IconAppWindow, IconHexagonNumber1, IconLayout2 } from "@tabler/icons-react";
 import MenuItem from "./MenuItem";
 
 export default function MenuPrincipal() {
 
     return (
         <aside className="w-80 bg-zinc-900">
-            <nav className="flex flex-col p-5 gap-1">
-                <MenuItem texto="Primeiro Componente" link="/primeiro"/>
-                <MenuItem texto="Flexbox" link="/flexbox"/>
-                <MenuItem texto="Component Pagina" link="/pagina"/>
+            <nav className="flex flex-col p-5">
+                <MenuItem link="/primeiro">
+                    <IconHexagonNumber1 />
+                    <span>Primeiro Component</span>
+                </MenuItem>
+                <MenuItem link="/flexbox">
+                    <IconLayout2 />
+                    <span>FlexBox</span>
+                </MenuItem>
+                <MenuItem link="/pagina">
+                    <IconAppWindow />
+                    <span>Component Página</span>
+                </MenuItem>
             </nav>
         </aside>
     )
