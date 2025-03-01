aefdb0d0311f4cbefc9ce8c8c351ee70566c2db2
Demetrius Carregosa <demetriuscarregosa@gmail.com>
2025-03-01 18:03:44 -0300

    create Rodape component

--- src/components/template/Pagina.tsx
+++ src/components/template/Pagina.tsx
@@ -1,5 +1,6 @@
 import Cabecalho from "./Cabecalho";
 import MenuPrincipal from "./MenuPrincipal";
+import Rodape from "./Rodape";
 
 export default function Pagina(props: any) {
     return (
@@ -7,9 +8,10 @@ export default function Pagina(props: any) {
             <Cabecalho />
             <div className="flex flex-grow">
                 <MenuPrincipal />
-                <main className="p-7 flex-1">
-                    {props.children}
-                </main>
+                <div className="flex flex-col flex-1">
+                    <main className="p-7 flex-1">{props.children}</main>
+                    <Rodape />
+                </div>
             </div>
         </div>
     )
