6b938f7eff52323cd6bd8126a239778359945acd
Demetrius Carregosa <demetriuscarregosa@gmail.com>
2025-02-28 23:40:55 -0300

    create MenuItem component

--- src/components/template/MenuPrincipal.tsx
+++ src/components/template/MenuPrincipal.tsx
@@ -1,12 +1,14 @@
-import Link from "next/link";
+
+import MenuItem from "./MenuItem";
 
 export default function MenuPrincipal() {
+
     return (
         <aside className="w-80 bg-zinc-900">
-            <nav className="flex flex-col p-5 gap-3">
-                <Link href="/primeiro">Primeiro Componente</Link>
-                <Link href="/flexbox">Flexbox</Link>
-                <Link href="/pagina">Component Pagina</Link>
+            <nav className="flex flex-col p-5 gap-1">
+                <MenuItem texto="Primeiro Componente" link="/primeiro"/>
+                <MenuItem texto="Flexbox" link="/flexbox"/>
+                <MenuItem texto="Component Pagina" link="/pagina"/>
             </nav>
         </aside>
     )
